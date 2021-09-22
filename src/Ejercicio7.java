33c24b89c52dfb0ad931e91c2cb8636457e94e41
bytesx <rockman$91>
2021-09-22 18:14:34 -0500

    Terminando Ejercicio 7

--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -3,8 +3,22 @@ import java.util.Scanner;
 public class Ejercicio7 {
     public static void main(String[] args) {
         // Ejercicio 7 - Java - Edgar Vazquez , Numeros Primos.
-        Scanner numPrimo = new Scanner(System.in);
 
+        // Solicitando Numero
+        Scanner num1 = new Scanner (System.in);
+        System.out.println("Entre un numero para comprobar si es numero primo");
+        boolean esPrimo = false;
+        int numPrimo = num1.nextInt();
 
+        for (int i = 2; i < numPrimo / 2; i++){
+            if (numPrimo % i == 0){
+                esPrimo = false;
+                System.out.println("No es numero primo");
+            }
+            else {
+                System.out.println("Es numero primo");
+            }
+
+        }
     }
 }
