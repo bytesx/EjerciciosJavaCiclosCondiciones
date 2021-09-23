98a41c2855dc51c40f599aec0f4063be3cace0c1
bytesx <rockman$91>
2021-09-22 20:54:04 -0500

    Iniciando Ejercicio 10

---
+++ src/Ejercicio10.java
@@ -0,0 +1,10 @@
+public class Ejercicio10 {
+    public static void main(String[] args) {
+
+        //Ejercicio 10 - Java - Edgar Vazquez
+        // Conocer Valor de la constante e "e =  1/0! + 1/1! + 1/2! + 1/3! + ..."
+
+        // Declarando Variables
+
+    }
+}
