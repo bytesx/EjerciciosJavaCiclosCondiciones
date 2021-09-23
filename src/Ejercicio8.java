966116889647115f25096179d33a6c5ce9e11fed
bytesx <rockman$91>
2021-09-22 19:05:38 -0500

    Avances de Ejercicio8

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -1,2 +1,17 @@
 public class Ejercicio8 {
+        public static void main(String[] args) {
+            // Ejercicio 8 - Java - Edgar Vazquez , Multiplicar 2 *2 hasta 4570
+
+            // Declarando Variables
+            int num1 = 2, num2 = 2, resultado = 0, resultado2;
+
+            for (resultado = 0; resultado < 4570;) {
+                resultado = num1 * num2;
+                if (resultado < 4570) {
+                   resultado2 = resultado * num1;
+                    System.out.println(resultado);
+                }
+            }
+
+        }
 }
