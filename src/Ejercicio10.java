6d84522cd67f0421f705b458853f7864d225e39b
bytesx <rockman$91>
2021-09-23 01:26:34 -0500

    Terminando el Ejercicio 10

--- src/Ejercicio10.java
+++ src/Ejercicio10.java
@@ -5,6 +5,13 @@ public class Ejercicio10 {
         // Conocer Valor de la constante e "e =  1/0! + 1/1! + 1/2! + 1/3! + ..."
 
         // Declarando Variables
+        float funcionN = 1, resultado =1, e = 10;
 
+        for (int i = 1; i <= e; i++) {
+            funcionN =(1 * i) * funcionN;
+            resultado = 1/funcionN + resultado;
+
+        }
+            System.out.println("La constante de e es: " + resultado);
     }
 }
