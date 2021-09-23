9c31dcc7a8620203ebcca4a87a30ccccd8abd443
bytesx <rockman$91>
2021-09-22 19:25:09 -0500

    Terminado y Probado Ejercicio 8

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -1,17 +1,13 @@
 public class Ejercicio8 {
         public static void main(String[] args) {
-            // Ejercicio 8 - Java - Edgar Vazquez , Multiplicar 2 *2 hasta 4570
+            // Ejercicio 8 - Java - Edgar Vazquez , Multiplicar 2 * hasta 4570
 
-            // Declarando Variables
-            int num1 = 2, num2 = 2, resultado = 0, resultado2;
 
-            for (resultado = 0; resultado < 4570;) {
-                resultado = num1 * num2;
-                if (resultado < 4570) {
-                   resultado2 = resultado * num1;
-                    System.out.println(resultado);
-                }
-            }
+            for (int multiplicador = 2, x = 2; x < 4570;) {
+             int resultado = x * multiplicador;
+             System.out.println(resultado);
+             x+=2;
+         }
 
         }
 }
